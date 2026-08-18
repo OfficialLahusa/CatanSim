6ecb33837e0732186cd2c0ba5733b248913571e1
Lasse Huber-Saffer <lasse@huber-saffer.de>
2026-08-18 20:35:19 +0200

    Refactoring

--- Agents/MCTS/MCTSAgent.cs
+++ Agents/MCTS/MCTSAgent.cs
@@ -21,6 +21,7 @@ namespace Agents.MCTS
 
         protected static StateValueNet _stateValueNet;
         protected static StateVectorizer _stateVectorizer;
+        
 
         public MCTSAgent(sbyte playerIndex, double explorationParameter = 1.414)
             : base(playerIndex)
@@ -36,7 +37,7 @@ namespace Agents.MCTS
 
         public override Action Act(GameState state, uint playedActionsCount)
         {
-            _tree = new MCTSTree(state);
+            _tree = new MCTSTree(state, PlayerIndex, _explorationParameter);
 
             RunForTime(TIME_LIMIT_SECONDS);
 
@@ -51,7 +52,6 @@ namespace Agents.MCTS
             for (int i = 0; i < iterations; i++)
             {
                 RunOneIteration();
-                //Console.WriteLine($"Completed {i+1}/{iterations}.");
             }
 
             TimeSpan runtime = stopwatch.Elapsed;
@@ -79,174 +79,23 @@ namespace Agents.MCTS
         {
             // Selection
             // Find a leaf node to expand, starting from the root node and traversing down the tree using the UCT formula
-            MCTSNode node = _tree.RootNode;
-            GameState state = new GameState(_tree.RootState);
-
-            while (node.Children.Count > 0)
-            {
-                node = SelectChild(node);
-
-                // Don't execute empty actions and actions leading into a randomness group (since their direct children are variations of that same action)
-                if (node.LastAction != null && !node.IsOutputRandomnessGroup)
-                {
-                    node.LastAction!.Apply(state);
-                }
-            }
+            (MCTSNode leafNode, GameState stateAtLeaf) = _tree.SelectLeafNode();
 
             // Expansion
-            // Add all children to leaf node if node is not terminal
-            if (!node.IsTerminal)
+            // If the selected leaf isn't terminal, expand its children and select a leaf node among them
+            if (!leafNode.IsTerminal)
             {
-                List<Action> actions = GetValidActions(state);
-                foreach (var action in actions)
-                {
-                    // If action has output randomness, get all possible outcomes and group them into a subtree
-                    if (action is IOutputRandomnessAction randomAction)
-                    {
-                        MCTSNode groupNode = new MCTSNode((sbyte)state.Turn.PlayerIndex, node, action, false, true);
-                        node.Children.Add(groupNode);
-
-                        List<Action> variants = randomAction.GetOutcomeVariants(state, (sbyte)state.Turn.PlayerIndex);
-
-                        foreach (Action variant in variants)
-                        {
-                            // Check if node is terminal and who the active player is after the action
-                            variant.Apply(state);
-                            bool isTerminal = state.HasEnded;
-                            sbyte nextActivePlayerIndex = (sbyte)state.Turn.PlayerIndex;
-                            variant.Revert(state);
-
-                            MCTSNode childNode = new MCTSNode(nextActivePlayerIndex, groupNode, variant, isTerminal);
-                            groupNode.Children.Add(childNode);
-                        }
-                    }
-                    // Otherwise just add the node directly as a child
-                    else
-                    {
-                        // Check if node is terminal and who the active player is after the action
-                        action.Apply(state);
-                        bool isTerminal = state.HasEnded;
-                        sbyte nextActivePlayerIndex = (sbyte)state.Turn.PlayerIndex;
-                        action.Revert(state);
-
-                        MCTSNode childNode = new MCTSNode(nextActivePlayerIndex, node, action, isTerminal);
-                        node.Children.Add(childNode);
-                    }
-                }
+                _tree.ExpandNode(leafNode, stateAtLeaf);
+                (leafNode, stateAtLeaf) = _tree.SelectLeafNode(leafNode, stateAtLeaf, suppressStateCopy: true);
             }
 
             // Simulation
-            // Select a child to simulate
-            MCTSNode nodeToSimulate;
-            if (!node.IsTerminal)
-            {
-                nodeToSimulate = SelectChild(node);
-
-                // Grouped output randomness node => Select child of child
-                if (nodeToSimulate.IsOutputRandomnessGroup)
-                {
-                    nodeToSimulate = SelectChild(nodeToSimulate);
-                    nodeToSimulate.LastAction!.Apply(state);
-                }
-                // Normal node => Select child
-                else
-                {
-                    nodeToSimulate.LastAction!.Apply(state);
-                }
-            }
-            else
-            {
-                nodeToSimulate = node;
-            }
-
             // Run simulation and get result (win percentage)
-            double result = SimulateML(state);
+            double result = SimulateML(stateAtLeaf);
 
             // Backpropagation
             // Update the total scores and visit counts for all nodes in the path from the simulated node back to the root
-            Backpropagate(nodeToSimulate, result);
-        }
-
-        protected MCTSNode SelectChild(MCTSNode node)
-        {
-            // If the node is a grouped output randomness action, choose a child node with uniform random distribution
-            if (node.IsOutputRandomnessGroup)
-            {
-                return node.Children[_random.Next(node.Children.Count)];
-            }
-
-            // Otherwise, determine if the player this agent is representing currently has deciding power over the next action
-            // A player has deciding power if one of the following is true:
-            // - It's the player's turn and nobody has to discard
-            // - The player has to discard and nobody with a lower playerIndex has to discard
-            bool isOwnersTurn = node.ActivePlayerIndex == PlayerIndex;
-            bool discardRequired = node.Children.Any(x => x.LastAction != null && x.LastAction is DiscardAction);
-            bool ownerHasToDiscard = node.Children.Any(x => x.LastAction != null && x.LastAction is DiscardAction discardAction && discardAction.PlayerIndex == PlayerIndex);
-            bool previousHasToDiscard = node.Children.Any(x => x.LastAction != null && x.LastAction is DiscardAction discardAction && discardAction.PlayerIndex < PlayerIndex);
-            bool hasDecidingPower = isOwnersTurn && !discardRequired || ownerHasToDiscard && !previousHasToDiscard;
-
-            // Only select best move if the player has deciding power, otherwise select random move.
-            // This is to avoid the AI from making intentional bad moves on other players' turns.
-            if (hasDecidingPower)
-            {
-                return node.Children.OrderByDescending(c => c.GetUCT(_explorationParameter)).First();
-            }
-            else
-            {
-                return node.Children[_random.Next(node.Children.Count)];
-            }
-        }
-
-        protected List<Action> GetValidActions(GameState state)
-        {
-            sbyte activePlayerIdx = (sbyte)state.Turn.PlayerIndex;
-
-            List<Action> firstInitialSettlementActions = FirstInitialSettlementAction.GetActionsForState(state, activePlayerIdx);
-            if (firstInitialSettlementActions.Count > 0)
-                return firstInitialSettlementActions;
-
-            List<Action> secondInitialSettlementActions = SecondInitialSettlementAction.GetActionsForState(state, activePlayerIdx);
-            if (secondInitialSettlementActions.Count > 0)
-                return secondInitialSettlementActions;
-
-            // Discards are assumed to be in player index order, since the outcome is order-invariant
-            // Therefore only the discards of the next player in order are shown
-            for (int playerToDiscardIdx = 0; playerToDiscardIdx < state.Players.Length; playerToDiscardIdx++)
-            {
-                if (DiscardAction.IsTurnValid(state.Turn, playerToDiscardIdx))
-                {
-                    List<Action> discardActions = DiscardAction.GetActionsForState(state, (sbyte)playerToDiscardIdx);
-
-                    if (discardActions.Count > 0)
-                        return discardActions;
-                }
-            }
-
-            List<Action> actions = [
-                .. EndTurnAction.GetActionsForState(state, activePlayerIdx),
-
-                .. RollAction.GetActionsForState(state, activePlayerIdx),
-                .. RobberAction.GetActionsForState(state, activePlayerIdx),
-
-                .. FirstInitialRoadAction.GetActionsForState(state, activePlayerIdx),
-                .. SecondInitialRoadAction.GetActionsForState(state, activePlayerIdx),
-
-                .. RoadAction.GetActionsForState(state, activePlayerIdx),
-                .. SettlementAction.GetActionsForState(state, activePlayerIdx),
-                .. CityAction.GetActionsForState(state, activePlayerIdx),
-                .. BuyDevelopmentCardAction.GetActionsForState(state, activePlayerIdx),
-
-                .. KnightAction.GetActionsForState(state, activePlayerIdx),
-                .. MonopolyAction.GetActionsForState(state, activePlayerIdx),
-                .. RoadBuildingAction.GetActionsForState(state, activePlayerIdx),
-                .. YearOfPlentyAction.GetActionsForState(state, activePlayerIdx),
-
-                .. FourToOneTradeAction.GetActionsForState(state, activePlayerIdx),
-                .. ThreeToOneTradeAction.GetActionsForState(state, activePlayerIdx),
-                .. TwoToOneTradeAction.GetActionsForState(state, activePlayerIdx)
-            ];
-
-            return actions;
+            _tree.Backpropagate(leafNode, result);
         }
 
         protected double SimulatePlayout(GameState state)
@@ -255,7 +104,7 @@ namespace Agents.MCTS
 
             while (!state.HasEnded)
             {
-                List<Action> actions = GetValidActions(state);
+                List<Action> actions = MCTSTree.GetValidActions(state);
                 if (actions.Count == 0)
                 {
                     Console.WriteLine("No actions available but not over: Saving state");
@@ -288,19 +137,6 @@ namespace Agents.MCTS
         {
             var inputTensor = _stateVectorizer.Vectorize(state, playedActionsCount);
             return _stateValueNet.Run(inputTensor);
-        }
-
-        protected void Backpropagate(MCTSNode node, double result)
-        {
-            MCTSNode? currentNode = node;
-
-            // Go up in tree until root is reached and add new result to each passed node
-            while (currentNode != null)
-            {
-                currentNode.VisitCount++;
-                currentNode.TotalScore += result;
-                currentNode = currentNode.Parent;
-            }
         }
     }
 }
--- Agents/MCTS/MCTSTree.cs
+++ Agents/MCTS/MCTSTree.cs
@@ -8,6 +8,7 @@ using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 using Action = Common.Actions.Action;
 
 namespace Agents.MCTS
@@ -16,12 +17,16 @@ namespace Agents.MCTS
     {
         public GameState RootState;
         public MCTSNode RootNode;
-        
+        protected sbyte _ownerIdx;
+        protected double _explorationParameter;
+        protected Random _random = new Random();
 
-        public MCTSTree(GameState rootState)
+        public MCTSTree(GameState rootState, sbyte ownerIdx, double explorationParameter)
         {
             RootState = rootState;
             RootNode = new MCTSNode((sbyte)rootState.Turn.PlayerIndex, null, null, false);
+            _ownerIdx = ownerIdx;
+            _explorationParameter = explorationParameter;
         }
 
         public Action GetMostPromisingMove()
@@ -50,6 +55,170 @@ namespace Agents.MCTS
                 sum += GetTreeSizeRec(child);
             }
             return sum;
+        }
+
+        public (MCTSNode leafNode, GameState stateAtNode) SelectLeafNode(MCTSNode? subtreeRoot = null, GameState? subtreeRootState = null, bool suppressStateCopy = false)
+        {
+            // If no subtree was specified, start from root of entire tree
+            MCTSNode node = subtreeRoot ?? RootNode;
+            // (Optimization) Suppress GameState deep copy if flag was set. This directly mutates the given subtree root state
+            GameState state = suppressStateCopy
+                ? subtreeRootState ?? new GameState(RootState)
+                : new GameState(subtreeRootState ?? RootState);
+
+            while (node.Children.Count > 0)
+            {
+                node = SelectChild(node);
+
+                // Don't execute empty actions and actions leading into a randomness group (since their direct children are variations of that same action)
+                if (node.LastAction != null && !node.IsOutputRandomnessGroup)
+                {
+                    node.LastAction!.Apply(state);
+                }
+            }
+
+            return (node, state);
+        }
+
+        public void ExpandNode(MCTSNode node, GameState state)
+        {
+            List<Action> actions = GetValidActions(state);
+            foreach (var action in actions)
+            {
+                // If action has output randomness, get all possible outcomes and group them into a subtree
+                if (action is IOutputRandomnessAction randomAction)
+                {
+                    MCTSNode groupNode = new MCTSNode((sbyte)state.Turn.PlayerIndex, node, action, false, true);
+                    node.Children.Add(groupNode);
+
+                    List<Action> variants = randomAction.GetOutcomeVariants(state, (sbyte)state.Turn.PlayerIndex);
+
+                    foreach (Action variant in variants)
+                    {
+                        // Check if node is terminal and who the active player is after the action
+                        variant.Apply(state);
+                        bool isTerminal = state.HasEnded;
+                        sbyte nextActivePlayerIndex = (sbyte)state.Turn.PlayerIndex;
+                        variant.Revert(state);
+
+                        MCTSNode childNode = new MCTSNode(nextActivePlayerIndex, groupNode, variant, isTerminal);
+                        groupNode.Children.Add(childNode);
+                    }
+                }
+                // Otherwise just add the node directly as a child
+                else
+                {
+                    // Check if node is terminal and who the active player is after the action
+                    action.Apply(state);
+                    bool isTerminal = state.HasEnded;
+                    sbyte nextActivePlayerIndex = (sbyte)state.Turn.PlayerIndex;
+                    action.Revert(state);
+
+                    MCTSNode childNode = new MCTSNode(nextActivePlayerIndex, node, action, isTerminal);
+                    node.Children.Add(childNode);
+                }
+            }
+        }
+
+        public void Backpropagate(MCTSNode node, double result)
+        {
+            MCTSNode? currentNode = node;
+
+            // Go up in tree until root is reached and add new result to each passed node
+            while (currentNode != null)
+            {
+                currentNode.VisitCount++;
+                currentNode.TotalScore += result;
+                currentNode = currentNode.Parent;
+            }
+        }
+
+        public MCTSNode SelectChild(MCTSNode node)
+        {
+            // If the node is a grouped output randomness action, choose a child node with uniform random distribution
+            if (node.IsOutputRandomnessGroup)
+            {
+                return node.Children[_random.Next(node.Children.Count)];
+            }
+
+            // Otherwise, determine if the player this agent is representing currently has deciding power over the next action            
+            // Only select best move if the player has deciding power
+            if (HasDecidingPower(node))
+            {
+                return node.Children.OrderByDescending(c => c.GetUCT(_explorationParameter)).First();
+            }
+            // Otherwise select random move
+            // This is to avoid the AI from making intentional bad moves on other players' turns
+            else
+            {
+                return node.Children[_random.Next(node.Children.Count)];
+            }
+        }
+
+        private bool HasDecidingPower(MCTSNode node)
+        {
+            // A player has deciding power if one of the following is true:
+            // - It's the player's turn and nobody has to discard
+            // - The player has to discard and nobody with a lower playerIndex has to discard
+            bool isOwnersTurn = node.ActivePlayerIndex == _ownerIdx;
+            bool discardRequired = node.Children.Any(x => x.LastAction != null && x.LastAction is DiscardAction);
+            bool ownerHasToDiscard = node.Children.Any(x => x.LastAction != null && x.LastAction is DiscardAction discardAction && discardAction.PlayerIndex == _ownerIdx);
+            bool previousHasToDiscard = node.Children.Any(x => x.LastAction != null && x.LastAction is DiscardAction discardAction && discardAction.PlayerIndex < _ownerIdx);
+
+            bool hasDecidingPower = isOwnersTurn && !discardRequired || ownerHasToDiscard && !previousHasToDiscard;
+            return hasDecidingPower;
+        }
+
+        public static List<Action> GetValidActions(GameState state)
+        {
+            sbyte activePlayerIdx = (sbyte)state.Turn.PlayerIndex;
+
+            List<Action> firstInitialSettlementActions = FirstInitialSettlementAction.GetActionsForState(state, activePlayerIdx);
+            if (firstInitialSettlementActions.Count > 0)
+                return firstInitialSettlementActions;
+
+            List<Action> secondInitialSettlementActions = SecondInitialSettlementAction.GetActionsForState(state, activePlayerIdx);
+            if (secondInitialSettlementActions.Count > 0)
+                return secondInitialSettlementActions;
+
+            // Discards are assumed to be in player index order, since the outcome is order-invariant
+            // Therefore only the discards of the next player in order are shown
+            for (int playerToDiscardIdx = 0; playerToDiscardIdx < state.Players.Length; playerToDiscardIdx++)
+            {
+                if (DiscardAction.IsTurnValid(state.Turn, playerToDiscardIdx))
+                {
+                    List<Action> discardActions = DiscardAction.GetActionsForState(state, (sbyte)playerToDiscardIdx);
+
+                    if (discardActions.Count > 0)
+                        return discardActions;
+                }
+            }
+
+            List<Action> actions = [
+                .. EndTurnAction.GetActionsForState(state, activePlayerIdx),
+
+                .. RollAction.GetActionsForState(state, activePlayerIdx),
+                .. RobberAction.GetActionsForState(state, activePlayerIdx),
+
+                .. FirstInitialRoadAction.GetActionsForState(state, activePlayerIdx),
+                .. SecondInitialRoadAction.GetActionsForState(state, activePlayerIdx),
+
+                .. RoadAction.GetActionsForState(state, activePlayerIdx),
+                .. SettlementAction.GetActionsForState(state, activePlayerIdx),
+                .. CityAction.GetActionsForState(state, activePlayerIdx),
+                .. BuyDevelopmentCardAction.GetActionsForState(state, activePlayerIdx),
+
+                .. KnightAction.GetActionsForState(state, activePlayerIdx),
+                .. MonopolyAction.GetActionsForState(state, activePlayerIdx),
+                .. RoadBuildingAction.GetActionsForState(state, activePlayerIdx),
+                .. YearOfPlentyAction.GetActionsForState(state, activePlayerIdx),
+
+                .. FourToOneTradeAction.GetActionsForState(state, activePlayerIdx),
+                .. ThreeToOneTradeAction.GetActionsForState(state, activePlayerIdx),
+                .. TwoToOneTradeAction.GetActionsForState(state, activePlayerIdx)
+            ];
+
+            return actions;
         }
     }
 }
